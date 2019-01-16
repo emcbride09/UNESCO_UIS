7da4d2ad46c029a81027629f02ee545525ced1fb
emcbride09 <elliot.a.mcbride@gmail.com>
2019-01-16 18:00:35 +0000

    all of africa

--- stamen_map_uis.R
+++ stamen_map_uis.R
@@ -58,19 +58,44 @@ uis_map_data_clip$year <- as.integer(uis_map_data_clip$year)
 
 #scrape Africa countries
 
+#rm(africa_df)
+#rm(africa)
+?as.data.frame
+
 url <- "http://www.worldometers.info/geography/how-many-countries-in-africa/"
 
 africa <- read_html(url) %>% 
   html_nodes("tr :nth-child(2)") %>% 
   html_text()
 
-africa <- africa %>% filter(africa != 'Country')
 
-##you can filter by multiple values but dont change to list or DF
+africa_names <- as.data.frame(africa, col.names = names('country'))
 
-uis_final <- uis_map_data_clip %>% filter(region %in% africa)
+africa_df <- africa_names %>% rename(country = africa) %>% 
+  filter(africa != "Country")
 
-uis_final$region <- as.factor(uis_final$region)
+
+#changedata frame
+##df.oil <- df.oil %>%  mutate(country = recode(country, `United States` = 'USA'
+
+
+africa_df <- africa_df %>% mutate( country = recode(country, `DR Congo` = 'Democratic Republic of the Congo',
+                                                    `Côte d'Ivoire` = "Ivory Coast",
+                                                    `Congo` = 'Republic of Congo')) %>% 
+  select(country)
+
+
+
+
+
+
+##you can filter by multiple values but dont change to list or DF keep as character vector
+
+africa_df <- as.character(africa_df$country)
+
+uis_final <- uis_map_data_clip %>% filter(region %in% africa_df)
+
+uis_final$region <- as.character(uis_final$region)
 
 
 
@@ -85,11 +110,21 @@ map <- get_stamenmap(c(bbox[1], bbox[2], bbox[3], bbox[4]), zoom = 4)
 # Build plot
 g <- ggmap(map)
 
-g + geom_polygon(data = uis_final, aes(x = long, y = lat, group = group, fill = rate), 
+anim <- g + geom_polygon(data = uis_final, aes(x = long, y = lat, group = group, fill = rate), 
                alpha = 0.8, color = "black") + 
-  labs(title = "National Rate of Out of School Children: {current_frame}") +
+  theme(plot.title = element_text(family = 'Arial', size = 20),
+        plot.subtitle = element_text(family = 'Arial', size = 16),
+        plot.background = element_rect(colour = 'black')) +
+  labs(title = "National Rate of Out of School Children", subtitle = "Year: {current_frame}") +
   scale_fill_gradientn(colours = c("palegreen", "orangered2")) +
-# Here come the gganimate specific bits 
+# Here come the gganimate specific bits <- changes result in erros usually mean the type of animation needs to change
   transition_manual(year) + 
   ease_aes('linear')
 
+anim
+
+anim_save("uis_anim.gif",anim)
+
+
+
+  
